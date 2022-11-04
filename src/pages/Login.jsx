0e633c9a0faf8c1e287d18eec741ab45754a37cb
estuardo sanchez <estuardo_101@hotmail.com>
2022-11-04 17:20:19 -0400

    create login/register form and add styling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='form-container'>
+    <div className='form-wrapper'>
+      <span className='logo'>FIGHTR</span>
+      <span className='title'>Login</span>
+      <form>
+        <input type='email' placeholder='email' />
+        <input type='password' placeholder='password' />
+        <button>Sign in</button>
+      </form>
+      <p>Don't have an account? Register</p>
+    </div>
+  </div>
   )
 }
 
