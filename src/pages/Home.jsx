7edfc9275a042dd700e45861ebf91018e4030b91
estuardo sanchez <estuardo_101@hotmail.com>
2022-11-05 23:52:35 -0400

    start on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='header'>
+      <h1 className='header-title'>
+        FIGHTR
+      </h1>
+    </div>
   )
 }
 
